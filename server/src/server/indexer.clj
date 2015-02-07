de55135db0a0245242ef93fd36eaa385606e8fa1
Andrey Butov <andreybutov@mail.ru>
2015-02-07 23:26:08 +0300

    Indexer saves data to a directory

--- server/src/server/indexer.clj
+++ server/src/server/indexer.clj
@@ -1,7 +1,26 @@
 (ns server.indexer
   (:require [clucy.core :as clucy]))
 
-(def index (clucy/memory-index))
+(def path "/tmp/news")
+
+(def index (clucy/disk-index path))
+
+(defn- safe-delete [file-path]
+  (if (.exists (clojure.java.io/file file-path))
+    (try
+      (clojure.java.io/delete-file file-path)
+      (catch Exception e (str "exception: " (.getMessage e))))
+    false))
+
+(defn- delete-directory [directory-path]
+  (let [directory-contents (file-seq (clojure.java.io/file directory-path))
+        files-to-delete (filter #(.isFile %) directory-contents)]
+    (doseq [file files-to-delete]
+      (safe-delete (.getPath file)))
+    (safe-delete directory-path)))
+
+(defn delete-all []
+  (delete-directory path))
 
 (defn insert [doc]
   (clucy/add index doc))
--- server/test/server/search_test.clj
+++ server/test/server/search_test.clj
@@ -8,14 +8,20 @@
 (defn- get-json-body [response]
   (decode (:body response) true))
 
+(def doc1 {:title "The first document" :url "http://github.com"})
+(def doc2 {:title "The second document" :url "http://twitter.com"})
+
 (deftest test-app
-  (indexer/insert {:title "The first document" :url "http://github.com"})
-  (indexer/insert {:title "The second document" :url "http://twitter.com"})
-  
+  (indexer/delete-all)
+  (indexer/insert doc1)
+  (indexer/insert doc2)
+
   (testing "search by phrase"
     (let [response (application (mock/request :post "/search" (encode {:keywords "first"})))]
       (is (= (:status response) 201))
-      (is (> (count (get-json-body response)) 0))))
+      (let [results (get-json-body response)]
+        (is (= (count results) 1))
+        (is (= (first results) doc1)))))
 
   (testing "empty result"
     (let [response (application (mock/request :post "/search" (encode {:keywords "unknown"})))]
