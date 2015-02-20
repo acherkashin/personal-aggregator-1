bcb83dec9db3d9e5a937ff9d54102b9b5215d31a
Andrey Butov <andreybutov@mail.ru>
2015-02-20 23:24:27 +0300

    Unnecessary files were deleted

--- rss-loader/src/main/scala/Starter.scala
+++
@@ -1,9 +0,0 @@
-import common._
-
-object Starter {
-  def main(args: Array[String]): Unit = {
-    println("You should implement reader for RSS list")
-    println("You should implement scheduler")
-    println("You should implement RSS loader")
-  }
-}
